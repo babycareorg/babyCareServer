bcd1136e22b822e90ace77ad1cf44f22e7dfaa55
kevin <songkun666@outlook.com>
2019-08-21 09:46:11 +0800

    init express

---
+++ server/config/develop.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mongodb: "mongodb://localhost:27017/isafe",
+    mailconfig: {
+        service: 'qq',
+        secure: true,
+        port: 465,
+        auth: {
+            user: '2586534184@qq.com',
+            pass: 'ghqvqsmanffbeabf'
+        }
+    }
+}
